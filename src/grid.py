04004c790f9dac3ff79db0e7758f5ed219e6a9db
Ner-Ber <neriberman@gmail.com>
2023-07-29 15:19:35 +0300

    Saving before reorganization, a bit messy

--- src/grid.py
+++ src/grid.py
@@ -1,14 +1,18 @@
+
 import numpy as np
 import grid_updaters
 
 DEFAULT_UPDATER = grid_updaters.NNCoulombFrictionUpdater(10, 0.5, 0.23)
 
 class BaseGrid:
-    def __init__(self, n, m=None, updater=DEFAULT_UPDATER):
+    def __init__(self, n, m=None, updater=DEFAULT_UPDATER, boundary_size=None):
         if m is None:
             m = n
         self.n = n
         self.m = m
+        if boundary_size is None:
+            boundary_size = int(np.ceil(0.1*n))
+        self.boundary_size = boundary_size
         self._create_cache()
         self._initialize_grid()
         self._save_to_cache(self.grid)
@@ -17,11 +21,12 @@ class BaseGrid:
     def run_n_steps(self, n):
         counter = 0
         n_sites = []
-        while counter < n+1:
-            updated_grid = self.updater.update_grid(self.grid)
-            avalanche_details = self.updater.avalanche_details
-            n_site_avalanched = len(avalanche_details['released_values'])
-            print(n_site_avalanched)
+        #TODO: implement the inside and clean boundary methods
+        while counter < n+1:    #TODO change to for loop
+            updated_grid = self.updater.update_step(self.grid)
+            observables = self.updater.avalanche_details
+            # n_site_avalanched = len(avalanche_details['released_values'])
+            n_site_avalanched = observables['released_values'] > 0
             if n_site_avalanched==0:
                 counter += 1
                 if counter >= n:
@@ -38,11 +43,49 @@ class BaseGrid:
         self.cache = np.empty((self.n, self.m, 0))
     
     def _initialize_grid(self):
-        self.grid = np.random.rand(self.n, self.m)
+        self.grid = np.random.rand(
+            self.n+self.boundary_size,
+            self.m+self.boundary_size
+            )
+        # self.grid = np.random.beta(1, 3, (self.n, self.m))
+
+    def _inside(self, array: np.ndarray):
+        return array[self.boundary_size:(-self.boundary_size), self.boundary_size:(-self.boundary_size)]
+
+    def clean_boundary_inplace(self, array: np.ndarray, fill_value = False) -> np.ndarray:
+        """
+        Fill `array` at the boundary with `fill_value`.
+
+        Useful to make sure sites on the borders do not become active and don't start toppling.
+
+        Works inplace - will modify the existing array!
+
+        :param array: array to be cleaned
+        :param fill_value: value to fill boundaries with
+        :rtype: np.ndarray
+        """
+        array[:self.boundary_size, :] = fill_value
+        array[-self.boundary_size:, :] = fill_value
+        array[:, :self.boundary_size] = fill_value
+        array[:, -self.boundary_size:] = fill_value
+        return array
+        
 
     def _save_to_cache(self, grid):
         self.cache = np.concatenate((self.cache, grid[:,:,None]), axis=2)
 
 
 if __name__ == '__main__':
-    pass
+    updater = grid_updaters.NNCoulombFrictionUpdater(f_s=3,
+                                                    increment=0.01,
+                                                    alpha=0,
+                                                    site_updater=grid_updaters._nn_update_ij_on_finite_bc,
+                                                    #  site_updater=grid_updaters._nn_update_ij_on_circular_bc,
+                                                    )
+    simple_grid = BaseGrid(200, updater=updater)
+    steps_in_iteration = 100
+    total_iterations = 10
+    n_sites = np.array([])
+    for _ in range(total_iterations):
+        n_sites = np.append(n_sites, simple_grid.run_n_steps(steps_in_iteration))
+        simple_grid.clear_cache()
--- src/grid_updaters.py
+++ src/grid_updaters.py
@@ -5,7 +5,7 @@ class GridUpdater:
         for k, v, in frictions_params.items():
             self.__setattr__(k, v)
 
-    def update_grid(self, grid: np.ndarray) -> np.ndarray:
+    def update_step(self, grid: np.ndarray) -> np.ndarray:
         return grid.copy()
 
 
@@ -20,7 +20,7 @@ class NNCoulombFrictionUpdater(GridUpdater):
         else:
             self.site_updater = site_updater
     
-    def update_grid(self, grid: np.ndarray) -> np.ndarray:
+    def update_step(self, grid: np.ndarray) -> np.ndarray:
         """Updates the grid according to a coulomb friction law. 
 
         If force sceeds threshold, will drop to 0 and distribute among neighbors.
@@ -36,20 +36,84 @@ class NNCoulombFrictionUpdater(GridUpdater):
             np.ndarray: the updated grid.
 
         """        
-        assert (grid.sum() < (grid.size * self.f_s)), "Sum of stess on grid exceeds maximal capacity"
-        exceed_logical = grid>=self.f_s
-        if np.any(exceed_logical):
-            return_grid = grid.copy()
-            for i, j in zip(*np.where(exceed_logical)):
-                self.site_updater(i, j, return_grid, self.alpha)
-            self.avalanche_details = {
-                'released_values': grid[exceed_logical]
-            }
-            return return_grid
-        else:
-            self.avalanche_details = {'released_values': []}
-            return grid + self.increment
-    
+
+        self.drive()
+        self.topple()
+
+        # assert (grid.sum() < (grid.size * self.f_s)), "Sum of stess on grid exceeds maximal capacity"
+        # exceed_logical = grid>=self.f_s
+        # if np.any(exceed_logical):
+            # return_grid = grid.copy()
+            # for i, j in zip(*np.where(exceed_logical)):
+                # self.site_updater(i, j, return_grid, self.alpha)
+            # self.avalanche_details = {
+                # 'released_values': grid[exceed_logical]
+            # }
+            # return return_grid
+        # else:
+            # self.avalanche_details = {'released_values': []}
+            # return grid + self.increment
+        
+        
+        # assert (grid.sum() < (grid.size * self.f_s)), "Sum of stess on grid exceeds maximal capacity"
+        # exceed_logical = grid>=self.f_s
+        # if np.any(exceed_logical):
+        #     i, j = np.unravel_index(np.argmax(grid), grid.shape)
+        #     return_grid = grid.copy()
+        #     return_grid = self.site_updater(i, j, return_grid, self.alpha)
+        #     self.avalanche_details = {
+        #         'released_values': grid[i, j],
+        #         'released_coords': (i, j),
+        #     }
+        #     return return_grid
+        # else:
+        #     self.avalanche_details = {
+        #         'released_values': 0,
+        #         'released_coords': None,
+        #         }
+        #     return grid + self.increment
+
+    def drive(self, grid):
+        return grid + self.increment
+
+    def topple(
+            values: np.ndarray,
+            visited: np.ndarray,
+            releases: np.ndarray,
+            critical_value_current: float,
+            critical_value: float,
+            conservation_lvl: float,
+            boundary_size: int
+            ) -> int:
+
+    # find a boolean array of active (overloaded) sites
+
+    active_sites = common.clean_boundary_inplace(
+        values >= critical_value_current, boundary_size)
+    number_of_iterations = 0
+
+    while active_sites.any():
+        
+        releases += active_sites
+        indices = np.vstack(np.where(active_sites)).T
+          # a Nx2 array of integer indices for overloaded sites
+        N = indices.shape[0]
+        for i in range(N):
+            x, y = index = indices[i]
+
+            neighbors = index + np.array([[0, 1], [-1, 0], [1, 0], [0,-1]])
+            for j in range(len(neighbors)):
+                xn, yn = neighbors[j]
+                values[xn, yn] += conservation_lvl * (values[x, y] - critical_value_current + critical_value)   # Grassberger (1994), eqns (1)
+                visited[xn, yn] = True
+
+            values[x, y] = critical_value_current - critical_value  # Grassberger (1994), eqns (1)
+            active_sites = common.clean_boundary_inplace(values >= critical_value_current, boundary_size)
+        number_of_iterations += 1
+
+    return number_of_iterations
+
+
 
 def _nn_update_ij_on_circular_bc(i, j, return_grid, alpha):
     n, m = return_grid.shape
@@ -59,6 +123,7 @@ def _nn_update_ij_on_circular_bc(i, j, return_grid, alpha):
     return_grid[i, j-1] += alpha*current_val
     return_grid[((i+1)%n), j] += alpha*current_val
     return_grid[i, ((j+1)%m)] += alpha*current_val
+    return return_grid
 
 
 def _nn_update_ij_on_finite_bc(i, j, return_grid, alpha):
@@ -73,6 +138,7 @@ def _nn_update_ij_on_finite_bc(i, j, return_grid, alpha):
         return_grid[i+1, j] += alpha*current_val
     if not (j+1>=m):
         return_grid[i, j+1] += alpha*current_val
+    return return_grid
 
 
 if __name__ == '__main__':
